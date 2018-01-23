2fd747ace9452921915db7faf66069b16002cd1d
Jan Lipecki <jan.lipecki@gmail.com>
2018-01-23 15:51:20 +0100

    Czyszczenie kodu

--- js/create-id.js
+++ js/create-id.js
@@ -1,17 +1,6 @@
+//.one - dana akcja będzie wykonana tylko raz
 $('#get-data').one('click', function() {
-//  console.log('Test - czy wyświetli się tylko raz?');
-//    document.body.createElement('div');
-//    $.create("div");
     var divNew = document.createElement('div');
     document.body.appendChild(divNew);
-    
     $('div:first').attr('id', 'dane-programisty');
-
-//         div.style.width = "100px";
-//     div.style.height = "100px";
-//     div.style.background = "red";
-//    document.getElementsByTagName('div').id = 'dane-programisty';
-//    $(HTMLDivElement).css('height', '25px');
-//    $(HTMLDivElement).css('background-color', 'red');
-
 });
